7331cd320b75f122b44f0bd0be55a48ab08c4897
Ravin Hettiarachchi <ravinhett11@gmail.com>
2019-06-12 16:36:12 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="player.php">
+    <div class="logo" width="10" height="20" background="#ffffff">
+      <span class = "badge badge-warning">
+        <img class="logo" src="images/logo.png" width="50" height="30" class="d-inline-block align-top" alt="" />
+      <b>1.0</b>
+      </span>
+    </div>
+  </a>
+  <div class="title">
+    <span class="badge badge-light">Listener.com</span>
+  </div>
+  <div class="results">
+    <span class="badge badge-warning"><a href="results.php">See Results</a></span>
+  </div>
+</nav>
